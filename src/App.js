f8f826098856321fcffc189f2fbb8c3c1da76d98
par333k <par333k@namver.com>
2020-11-25 23:45:54 +0900

    movie component css api get data create

--- src/App.js
+++ src/App.js
@@ -1,63 +1,56 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-function Food({ name, picture, rating }) {
-return (
-  <div>
-    <h2>I like { name }</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={ picture } alt={name} />
-  </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movie: [],
+  };
 
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'https://image5jvqbd.fmkorea.com/files/attach/new/20201123/3842645/2114102785/3217937505/c3f988c8a1923a1776f2d172cac9caeb.jpg',
-    rating: 5,
-  },
-  { 
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg',
-    rating: 4.9,
-  },
-  { 
-    id: 3,
-    name: 'Bibimbob',
-    image: 'https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg',
-    rating: 3,
-  },
-  { 
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg',
-    rating: 1,
-  },
-  { 
-    id: 5,
-    name: 'Kimbap',
-    image: 'https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg',
-    rating: 4,
+  getMovies = async () => {
+    const {
+      data : {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+  //  this.setState({ movies: movies })
+  // ES6에서는 객체의 키와 대입할 변수의 이름이 같다면 코드 축약이 가능
+    this.setState({ movies, isLoading: false });
   }
-];
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key = {dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating} />
-        ))}
-    </div>
-  );
+  componentDidMount() {
+    this.getMovies();
+  }
+  
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading 
+          ? (
+            <div className="loader">
+              <span className="loader__text">'Loading...'</span> 
+            </div>
+          ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie 
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div> 
+            )}
+      </section>
+    );
+  }
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
 
 export default App;
